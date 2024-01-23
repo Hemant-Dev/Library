56c51c46d4794d572c0402460f83cfb59d9d8e4f
Hemant-Dev <phemant092@gmail.com>
2024-01-23 22:08:41 +0530

    Big Fix: 1.Fixed Add book bug 2.Fixed Change Status and Delete Buttons 3.By Adding a onClick event while creating a obj of it JS

--- script.js
+++ script.js
@@ -7,41 +7,67 @@ const closeBtn = document.querySelector('dialog button');
 const showBtn = document.querySelector('.add-card-btn');
 const submitBtn = document.querySelector('#submit-btn');
 let cardsCount = 0; //Global Var to keep track of 
-let cardsList = getCardList();
+
 // Test Data
-let book = new Book("Demo", "Auth", 200, true);
-let b1 = new Book("B1", "Auth1", 100, false);
-let b2 = new Book("B2", "Auth2", 200, true);
-let b3 = new Book("B3", "Auth3", 400, false);
+// let book = new Book("Demo", "Auth", 200, true);
+// let b1 = new Book("B1", "Auth1", 100, false);
+// let b2 = new Book("B2", "Auth2", 200, true);
+// let b3 = new Book("B3", "Auth3", 400, false);
 //Seeding the data
-addBookToLibrary(b1, myLibrary);
-addBookToLibrary(b2, myLibrary);
-addBookToLibrary(book, myLibrary);
-addBookToLibrary(b3, myLibrary);
-addBookToLibrary(b3, myLibrary);
+// addBookToLibrary(b1, myLibrary);
+// addBookToLibrary(b2, myLibrary);
+// addBookToLibrary(book, myLibrary);
+// addBookToLibrary(b3, myLibrary);
+// addBookToLibrary(b3, myLibrary);
 
 //Initializing the library
-showLibrary(myLibrary);
+// showLibrary(myLibrary);
 
 // Functions to create the book card
 function createCard(bookObj) {
     const card = document.createElement('div');
     addCardElements(card, bookObj);
-    cardsCount++;  //Updating Total Cards Count
+    //Updating Total Cards Count
     card.classList = "card";
-    card.setAttribute("data-card-index", cardsCount);
-    bookContainer.insertBefore(card, addBookCard);
-    cardsList = getCardList();
-    addingEventListenersOnEachCards();
+    // cardsCount++;
+    // card.setAttribute("data-card-index", cardsCount);
+    // bookContainer.insertBefore(card, addBookCard);
+    bookContainer.appendChild(card);
 }
 
 // Function to show the already created library
-function showLibrary(myLibrary) {
-    myLibrary.forEach((object) => {
-        createCard(object);
-    });
+function updateBooksGrid(){
+    resetBookGrid();
+    for(let book of myLibrary){
+        createCard(book);
+    }
 }
 
+function removeBook(e){
+    const title = e.target.parentNode.children[0].firstChild.data;
+    // console.log(title);
+    const book = getBookFromLibrary(title);
+    const index = myLibrary.indexOf(book);
+    removeBookFromLibrary(index);
+    updateBooksGrid();
+}
+
+function changeStatus(e){
+    const title = e.target.parentNode.children[0].firstChild.data;
+    // console.log(getBook(cardIndex).isRead);
+    const book = getBookFromLibrary(title);
+    book.isRead = !book.isRead;
+    updateBooksGrid();
+}
+function removeBookFromLibrary(index){
+    myLibrary.splice(index, 1);
+}
+function getBookFromLibrary(title){
+    return myLibrary.find((book) => book.title === title);
+}
+function isInLibrary(newBook){
+    return myLibrary.some((book) => book.title === newBook.title)
+}
 //Function to show the book porperties in the card
 function addCardElements(card, bookObj) {
     const title = document.createElement('h3');
@@ -52,19 +78,26 @@ function addCardElements(card, bookObj) {
     pages.classList.add('page-no');
     const status = document.createElement('h4');
     status.classList.add('status');
+    const statusBtn = document.createElement('button');
+    statusBtn.classList.add('delete-btn', 'status-btn');
     const delBtn = document.createElement('button');
     delBtn.classList.add('delete-btn');
+
+    delBtn.onclick = removeBook;
+    statusBtn.onclick = changeStatus;
 
     title.textContent = bookObj.title;
     author.textContent = bookObj.author;
     pages.textContent = "Pages: " + bookObj.noOfPages;
     status.textContent = "Read: " + bookObj.isRead;
+    statusBtn.textContent = "Change";
     delBtn.textContent = "Delete";
 
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
     card.appendChild(status);
+    card.appendChild(statusBtn);
     card.appendChild(delBtn);
 }
 
@@ -86,31 +119,17 @@ submitBtn.addEventListener('click', (event) => {
         const bookObj = new Book(title.value, author.value, pages.value, status.value);
         // console.log(bookObj);
         addBookToLibrary(bookObj, myLibrary);
-        createCard(bookObj);
-        console.log(myLibrary);
+        // createCard(bookObj);
+        updateBooksGrid();
+        console.log(myLibrary); 
         dialog.close();
     }
 });
 
-function removeCard(cardIndex){
-    // console.log(myLibrary);
-    myLibrary.splice(cardIndex-1, 1);
-    cardsCount--;
-    // console.log(myLibrary);
-}
-function getCardList(){
-    return Array.from(document.querySelectorAll('[data-card-index]'));
-}
+// Reset Grid
+const deleteBtn = document.querySelector('.delete-btn');
+deleteBtn.addEventListener('click', resetBookGrid);
 
-// Deletes any card the user clicks
-function addingEventListenersOnEachCards(){
-    // Array.from(cardsList);
-    cardsList.forEach((card) => {
-        card.lastChild.addEventListener('click', () => {
-            // console.log(card);
-            card.remove();
-            removeCard(card.getAttribute('data-card-index'));   // Removes the card obj from the library arr
-        });
-    });
+function resetBookGrid(){
+    bookContainer.innerHTML = '';
 }
-
